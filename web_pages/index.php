8c8420548a42c0483faaad0335a8698c4db72aea
volpepe <volpepe.prog@gmail.com>
2019-08-10 03:27:41 +0200

    it's late

---
+++ web_pages/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="mainstyle.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>RolePlan: Main Page</title>
+</head>
+<body>
+    <div id="container">
+        <div class="master-side">
+            <button id="master-button" class="btn btn-primary"><i class="fas fa-crown"></i> Accesso Master</button>
+        </div>
+        <div class="player-side">
+            <button id="player-button" class="btn btn-primary"><i class="fas fa-user"></i> Accesso Giocatore</button>
+        </div>
+    </div>
+</body>
+</html>
+
+<script>
+$(document).ready(function(){
+    $("#player-button").click(function(){
+        window.location = "playermenu.php"
+    })
+    $("#master-button").click(function(){
+        window.location = "mastermenu.php"
+    })
+})
+</script>
